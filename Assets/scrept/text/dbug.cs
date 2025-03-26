06219c2cc99c77cb9f099397106e4c73b5f54991
lovestar0913 <starnightlin@gmail.com>
2025-03-27 01:25:55 +0900

    999

---
+++ Assets/scrept/text/dbug.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TaskWithCancelTest : MonoBehaviour
+{
+    private CancellationTokenSource cancellationTokenSource;
+
+    void Start()
+    {
+        StartLogging();
+    }
+
+    public void StartLogging()
+    {
+        StopLogging(); // 既に実行中なら停止（多重起動を防ぐ）
+        cancellationTokenSource = new CancellationTokenSource();
+        Task.Run(() => LogEverySecond(cancellationTokenSource.Token));
+    }
+
+    async Task LogEverySecond(CancellationToken token)
+    {
+        try
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Debug.Log("Task 実行中：" + Time.time);
+                await Task.Delay(1000, token); // キャンセル可能な遅延
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            Debug.Log("Task がキャンセルされました");
+        }
+        finally
+        {
+            Debug.Log("Task ループが完全に終了しました");
+        }
+    }
+
+    public void StopLogging()
+    {
+        if (cancellationTokenSource != null)
+        {
+            cancellationTokenSource.Cancel(); // タスクをキャンセル
+            cancellationTokenSource.Dispose();
+            cancellationTokenSource = null;
+            Debug.Log("Task 停止！");
+        }
+    }
+}
